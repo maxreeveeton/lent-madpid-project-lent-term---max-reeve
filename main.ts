3845e2d7bd718668081fc2ab61f95aeaad4fd2cc
reevemax <77487161+reevemax@users.noreply.github.com>
2021-01-29 10:15:32 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,30 @@
-scene.setBackgroundColor(7)
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 
+        . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 
+        . . . 9 9 9 9 9 9 9 9 9 9 9 9 9 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, projectile, 200, 200)
+})
+let mySprite: Sprite = null
+let projectile: Sprite = null
+let XWing: Sprite = null
+scene.setBackgroundColor(12)
 effects.starField.startScreenEffect()
-let mySprite = sprites.create(img`
+controller.moveSprite(XWing, 200, 200)
+XWing = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . 5 5 5 5 8 . . . . . . . . 
     . . . . . . . f 8 . . . . . . . 
@@ -18,4 +42,24 @@ let mySprite = sprites.create(img`
     . . . 5 5 5 5 8 . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+info.setLife(3)
+game.onUpdateInterval(500, function () {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . f f f . 
+        . . . . . . . . . . . f f 9 9 f 
+        . . . . 7 f 7 f 7 f 7 f 9 9 9 f 
+        . . . f a a a a a a a a a a a f 
+        . . f 7 f f f f f f f f f f f f 
+        . . . f a a a a a a a a a a f . 
+        . . . . 7 f 7 f 7 f 7 f f f 9 9 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+})
